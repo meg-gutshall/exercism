b9ec6e5f7d6e13fcadcb538689f35fcbdebcaa21
Meg Gutshall <meghan.gutshall@gmail.com>
2020-11-29 14:58:09 -0500

    REAL refactor

--- teams/virtual-coffee/ruby/space-age/space_age.rb
+++ teams/virtual-coffee/ruby/space-age/space_age.rb
@@ -15,17 +15,21 @@ class SpaceAge
   end
 
   # REFACTORED CODE
+  def method_missing(method_id)
+    calculate_age(method_id)
+  end
 
   def calculate_age(method_name)
+    # method_name is a symbol
     one_year = {
-      on_earth: 31557600.00,
-      on_mercury: one_year[on_earth] * 0.2408467,
-      on_venus: one_year[on_earth] * 0.61519726,
-      on_mars: one_year[on_earth] * 1.8808158,
-      on_jupiter: one_year[on_earth] * 11.862615,
-      on_saturn: one_year[on_earth] * 29.447498,
-      on_uranus: one_year[on_earth] * 84.016846,
-      on_neptune: one_year[on_earth] * 164.79132
+      on_earth: @earth_year,
+      on_mercury: @earth_year * 0.2408467,
+      on_venus: @earth_year * 0.61519726,
+      on_mars: @earth_year * 1.8808158,
+      on_jupiter: @earth_year * 11.862615,
+      on_saturn: @earth_year * 29.447498,
+      on_uranus: @earth_year * 84.016846,
+      on_neptune: @earth_year * 164.79132
     }
 
     @seconds / one_year[method_name]
@@ -33,35 +37,35 @@ class SpaceAge
 
   # ORIGINAL SOLUTION
 
-  def on_earth
-    @seconds / @earth_year
-  end
+  # def on_earth
+  #   @seconds / @earth_year
+  # end
 
-  def on_mercury
-    @seconds / (@earth_year * 0.2408467)
-  end
+  # def on_mercury
+  #   @seconds / (@earth_year * 0.2408467)
+  # end
 
-  def on_venus
-    @seconds / (@earth_year * 0.61519726)
-  end
+  # def on_venus
+  #   @seconds / (@earth_year * 0.61519726)
+  # end
 
-  def on_mars
-    @seconds / (@earth_year * 1.8808158)
-  end
+  # def on_mars
+  #   @seconds / (@earth_year * 1.8808158)
+  # end
 
-  def on_jupiter
-    @seconds / (@earth_year * 11.862615)
-  end
+  # def on_jupiter
+  #   @seconds / (@earth_year * 11.862615)
+  # end
 
-  def on_saturn
-    @seconds / (@earth_year * 29.447498)
-  end
+  # def on_saturn
+  #   @seconds / (@earth_year * 29.447498)
+  # end
 
-  def on_uranus
-    @seconds / (@earth_year * 84.016846)
-  end
+  # def on_uranus
+  #   @seconds / (@earth_year * 84.016846)
+  # end
 
-  def on_neptune
-    @seconds / (@earth_year * 164.79132)
-  end
+  # def on_neptune
+  #   @seconds / (@earth_year * 164.79132)
+  # end
 end
